5f49eebb3d446eb6f834e75edb77b26e630c1cad
Anita Katakkar <you@example.com>
2025-09-23 10:35:39 -0400

    changing enum to a type

--- src/enums/Gender.tsx
+++ src/enums/Gender.tsx
@@ -1,7 +1,14 @@
-export enum Gender {
-  male = "male",
-  female = "female",
-  nonBinary = "non-binary",
-  transgender = "transgender",
-  agender = "agender",
-}
+// export enum Gender {
+//   male = "male",
+//   female = "female",
+//   nonBinary = "non-binary",
+//   transgender = "transgender",
+//   agender = "agender",
+// }
+
+export type Gender =
+  | "male"
+  | "female"
+  | "nonBinary"
+  | "transgender"
+  | "agender";
