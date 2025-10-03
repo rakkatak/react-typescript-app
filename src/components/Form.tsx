9e969960e1f6c3ded20e4633e99e9b7af1a24165
Anita Katakkar <you@example.com>
2025-10-03 12:06:48 -0400

    added initial files

---
+++ src/components/Form.tsx
@@ -0,0 +1,12 @@
+const Form = () => {
+  return (
+    <>
+      <form>
+        <label htmlFor="name" className="form-label"></label>
+        <input id="name" type="text" className="form-control"></input>
+      </form>
+    </>
+  );
+};
+
+export default Form;
